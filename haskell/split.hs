fd15ba9fd597678a034a87e78fee4f8175bb4f9c
SheriefBadran <sb222rf@student.lnu.se>
2016-03-24 11:10:55 +0100

    fix mistake

--- haskell/split.hs
+++ haskell/split.hs
@@ -9,14 +9,14 @@ split' p [] = ([],[])
 split' p (x:xs)
     | p x = (x : ys, zs)
     | not $ p x = (ys, x : zs)
-  where (ys, zs) = split p xs
+  where (ys, zs) = split' p xs
 
 split'' :: (Int -> Bool) -> [Int] -> ([Int], [Int])
 split'' p [] = ([],[])
 split'' p (x:xs)
     | p x = (x : ys, zs)
     | otherwise = (ys, x : zs)
-  where (ys, zs) = split p xs
+  where (ys, zs) = split'' p xs
 
   isEven :: Int -> Bool
   isEven number = number `mod` 2 == 0
