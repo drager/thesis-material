cae9b8ff40d8e94e954c1a6859aa89c15753426c
SheriefBadran <sb222rf@student.lnu.se>
2016-04-12 16:25:14 +0200

    Cleaning up functional split file

--- functional-javascript/split.js
+++ functional-javascript/split.js
@@ -1,21 +1,5 @@
 'use strict'
 
-// const split = xs => {
-//   const even = []
-//   const odd = []
-//
-//   for (let i = 0; i < xs.length; i++) {
-//     if (xs[i] % 2 === 0) {
-//       even.push(xs[i])
-//     } else {
-//       odd.push(xs[i])
-//     }
-//   }
-//
-//   return [even, odd]
-// }
-// const result = split([1, 2, 3, 4, 5])
-
 const isEven = x => x % 2 === 0
 const split = (isEven, list) => [list.filter(n => isEven(n)), list.filter(n => !isEven(n))]
 const result = split(isEven, [1, 2, 3, 4, 5])
