1af11b56e7faece7000ea442f68f5d87ec3ccf60
Jesper Håkansson <drager@programmers.se>
2016-05-03 15:15:11 +0200

    Fix accumArray, add head function and remove unused

--- functional-javascript/graph-haskell-translated/graph.js
+++ functional-javascript/graph-haskell-translated/graph.js
@@ -1,44 +1,35 @@
-
 import {compose} from '../graph/graph'
-import {indices, accumArray} from './helpers'
+import {accumArray, head, indices} from './helpers'
 
 // buildG :: Bounds -> [Edge] -> Graph
-const buildG = edges0 => {
-  const graph = accumArray(edges0)
-  return Object.keys(graph).map(key => ({[key]: graph[key]}))
-}
-// console.log(buildG([[50, 51], [50, 52], [52, 53], [53, 54]]))
-console.log('graph: ', buildG([[50, 51], [50, 52], [50, 54], [52, 53]]))
+const buildG = edges0 => accumArray(edges0)
+// console.log('graph: ', buildG([[50, 51], [50, 52], [50, 54], [52, 53]]))
 
 // edges :: Graph -> [Edge]
 const edges = g => g
   .map(e => e[Object.keys(e)]
-    .map(v => [Object.keys(e)[0], v]))
+    .map(v => [head(Object.keys(e)), v]))
   .filter(v => v.length > 0)
   .reduce((acc, curr) => acc.concat(curr))
 
-edges(buildG([[50, 51], [50, 52], [52, 53], [53, 54]]))
-
+// vertices :: Graph -> [Vertex]
 const vertices = indices
-// const result = vertices(buildG([[50, 51], [50, 52], [52, 53], [53, 54]]))
 
-// [{'50': [54,52,51]}, {'51': []}, {'52': [53]}, {'53': []}, {'54': []}] => [{54: 50}, {52: 50}, {51: 50}, {53: 52}]
 // reverseE :: Graph -> [Edge]
 const reverseE = g => edges(g).map(edge => [...edge.reverse()])
-console.log('reverseE: ', reverseE(buildG([[50, 51], [50, 52], [50, 54], [52, 53]])))
+// console.log('reverseE: ', reverseE(buildG([[50, 51], [50, 52], [50, 54], [52, 53]])))
 
 // transposeG  :: Graph -> Graph
 const transposeG = g => compose(buildG, reverseE)(g)
-console.log('transposeG: ', transposeG(buildG([[50, 51], [50, 52], [50, 54], [52, 53]])))
-// console.log('graph: ', transposeG(buildG([[50, 51], [50, 52], [50, 54], [52, 53]])))
+// console.log('transposeG: ', transposeG(buildG([[50, 51], [50, 52], [50, 54], [52, 53]])))
 
+// mapTable :: (Vertex -> a -> b) -> Table a -> Table b
 const mapTable = f => g => indices(g).map((v, i) => [v, f(g[i][v])])
 
+// outdegree :: Graph -> Table Int
 const outdegree = mapTable((ws) => ws.length)
-console.log('outdegree: ', outdegree(buildG([[50, 51], [50, 52], [50, 54], [52, 53]])))
+// console.log('outdegree: ', outdegree(buildG([[50, 51], [50, 52], [50, 54], [52, 53]])))
 
+// indegree :: Graph -> Table Int
 const indegree = compose(outdegree, transposeG)
-console.log('indegree: ', indegree(buildG([[50, 51], [50, 52], [50, 54], [52, 53]])))
-// console.log(accumArray(reverseE(buildG([[50, 51], [50, 52], [50, 54], [52, 53]])))) // debugg indegree
-// reverseE(buildG([[50, 51], [50, 52], [50, 54], [52, 53]]))
-// console.log(reverseE(buildG([[50, 51], [50, 52], [50, 54], [52, 53]])))
+// console.log('indegree: ', indegree(buildG([[50, 51], [50, 52], [50, 54], [52, 53]])))
--- functional-javascript/graph-haskell-translated/helpers.js
+++ functional-javascript/graph-haskell-translated/helpers.js
@@ -1,14 +1,19 @@
 export const indices = g => g.reduce((acc, v) => acc.concat(...Object.keys(v)), [])
 
-export const accumArray = edges0 => edges0.reduce((acc, edge) => {
-    !acc.hasOwnProperty(edge[0]) ?
-      acc[edge[0]] = [].concat([edge[1]]) :
+export const accumArray = edges0 => {
+  // Each key value in {} corresponds to a tuple
+  const edges = edges0.reduce((acc, edge) => {
+    if (!acc.hasOwnProperty(edge[0])) {
+      acc[edge[0]] = [edge[1]]
+    } else {
       acc[edge[0]] = acc[edge[0]].concat([edge[1]])
-
-      if (!acc[edge[0]] || !acc[edge[1]]) {
-        acc[edge[1]] = []
-        return acc
-      }
+    }
+    acc[edge[1]] = []
 
     return acc
   }, {})
+
+  return Object.keys(edges).map(key => ({[key]: edges[key]}))
+}
+
+export const head = a => a[0]
